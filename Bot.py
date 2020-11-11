0a7e4b2ad84ace274fc215e322bec0015b32df99
Noham Hirep <noham.hirep@hetic.net>
2020-11-12 00:38:57 +0100

    Premiere version

--- Bot.py
+++ Bot.py
@@ -30,13 +30,12 @@ def order():
     add_to_cart = driver.find_element_by_xpath('//*[@id="add-remove-buttons"]/input')
     add_to_cart.click()
 
-    # attendre le chargement de la page
+    # wait for the page to load
     time.sleep(.5)
     checkout_element = driver.find_element_by_xpath('//*[@id="cart"]/a[2]')
     checkout_element.click()
 
-    # infos
-
+    # Writing information
     driver.find_element_by_xpath('//*[@id="order_billing_name"]').send_keys(config['name'])
     driver.find_element_by_xpath('//*[@id="order_email"]').send_keys(config['email'])
     driver.find_element_by_xpath('//*[@id="order_tel"]').send_keys(config['phone'])
