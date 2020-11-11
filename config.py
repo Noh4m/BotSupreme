6af0c85211cb4a69382b165d7f9770ce963f64b3
Noham Hirep <noham.hirep@hetic.net>
2020-11-11 19:50:30 +0100

    premiere version du bot

---
+++ config.py
@@ -0,0 +1,15 @@
+config = {
+            "product_url": "https://www.supremenewyork.com/shop/sweatshirts/bk2i64cnx/j5zieajxh",
+            "name": input("Name"),
+            "email": input("Email"),
+            "phone": input("Phone"),
+            "adresse": input("Adresses"),
+            "city": input("City"),
+            "zipCode": input("ZipCode"),
+            "cardNumber": input("CardNumber"),
+            "cardCvv": input("Cvv"),
+
+
+
+
+}
